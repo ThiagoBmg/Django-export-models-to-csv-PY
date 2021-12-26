23e0bb9d80f385f27032c0629ee26092f3b573d6
ThiagoBmg <th.bmg5274@gmail.com>
2021-12-26 14:41:25 -0300

    chore: refatorando

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 
 from genericpath import isdir, isfile
-from os import listdir, system
+from os import listdir, system, terminal_size
 from posixpath import join
+import logging 
+
+log = logging.getLogger(__name__)
+logging.basicConfig(filename='service.log')
 
 MODELS = list() 
 """
@@ -10,14 +14,25 @@ TODO:
 
 """
 def extract_values(text:str):  
-    text = text.split(' = models.')
-    fild_name = text[0]
-    text_ = text[1].split('(')
-    fild_type  = text_[0]
-    fild_description = text_[1].replace(')', '')
-    fild_description = fild_description if len(fild_description) != 1  else '-'
-    return fild_name, fild_type, fild_description
+    #TODO: MELHORA
+    try:
+        
+        text_ = text.split(' = models.')
+        if len(text_) == 1:
+            text_ = text.split('=models.')
+        if len(text_) == 1:
+            text_ = text.split(' =  models.')
 
+        fild_name = text_[0]
+        text_ = text_[1].split('(')
+        fild_type  = text_[0]
+        fild_description = text_[1].replace(')', '')
+        fild_description = fild_description if len(fild_description) != 1  else '-'
+        return fild_name, fild_type, fild_description
+    except Exception as exc:
+        #print(text.split(' = models.')[0])
+        return '', '', ''
+    
 def extract_values2(text:str):
     try:
         text = text.split('= ')
@@ -34,43 +49,46 @@ def run2(FILE_NAME:str):
 
     with open(FILE_NAME,  'r') as file: 
         lines = file.readlines()
-        for line in lines:        
-            if 'class' in line and 'models.Model' in line:
-                table_name = (line.split("class")[1]).split("(")[0]
-                qtd_models += 1
-            elif 'models.' in line \
-                and not ('models.Model') in line \
-                and '=' in line \
-                and not 'Index' in line:
+        for line in lines:
+            try:
+                if 'class' in line and 'models.Model' in line:
+                    table_name = (line.split("class")[1]).split("(")[0]
+                    qtd_models += 1
+                elif 'models.' in line \
+                    and not ('models.Model') in line \
+                    and '=' in line \
+                    and not 'Index' in line:
 
-                qtd_filds += 1
+                    qtd_filds += 1
 
-                if '(' in line and ')' in line:
-                    f_name, res, desc = extract_values(line)
-                    t.append({
-                        "table_name": table_name.replace(" ",""),
-                        "fild_name":f_name.replace(" ",""),
-                        "fild_type":res.replace(" ",""),
-                        "desc":desc.replace("\n","")
-                    }) if f_name.replace(" ","") != 'N' else ...
-                else:
-                    d  = extract_values2(line)
-                    if d:
+                    if '(' in line and ')' in line:
+                        f_name, res, desc = extract_values(line)
                         t.append({
                             "table_name": table_name.replace(" ",""),
-                            "fild_name":d[0].replace(" ",""),
-                            "fild_type":d[1].replace(" ",""),
-                            "desc":'-'.replace("\n","")
-                        }) if d[0].replace("","") != 'N' else ...
-
+                            "fild_name":f_name.replace(" ",""),
+                            "fild_type":res.replace(" ",""),
+                            "desc":desc.replace("\n","")
+                        }) if f_name.replace(" ","") != 'N' else ...
+                    else:
+                        d  = extract_values2(line)
+                        if d:
+                            t.append({
+                                "table_name": table_name.replace(" ",""),
+                                "fild_name":d[0].replace(" ",""),
+                                "fild_type":d[1].replace(" ",""),
+                                "desc":'-'.replace("\n","")
+                            }) if d[0].replace("","") != 'N' else ...
+            except Exception as exc:
+                log.error(f'{FILE_NAME} - [ERRO AO FORMATAR CAMPO] {exc} - line: {line}')
+            
     with open(FILE_NAME.replace('txt', 'csv').replace("extract_doc","extract_doc/csv"), 'w') as file:
         for i in t:
             file.writelines(f'{i.get("table_name")};')
             file.writelines(f'{i.get("fild_name")};')
             file.writelines(f'{i.get("fild_type")};')
             file.writelines(f'{i.get("desc")}\n')
-    #print(f'\n{FILE_NAME} - Quantidade de Tabelas localizadas {qtd_models}\n')
-    #print(f'{FILE_NAME} - Quantidade de campos localizados {qtd_filds}')
+    log.warning(f'{FILE_NAME} - Quantidade de Tabelas localizadas {qtd_models}')
+    log.warning(f'{FILE_NAME} - Quantidade de campos localizados {qtd_filds}')
 
 """
 
@@ -78,7 +96,7 @@ TODO:
 
 """
 def get_files_from_dir(dir:str):
-    print(dir)
+    #print(dir)
     files = [file for file in listdir(f'../{dir}') if isfile(join(f'../{dir}',file))]
     dirs = [file for file in listdir(f'../{dir}') if isdir(join(f'../{dir}',file))]
     return files , dirs
@@ -130,11 +148,12 @@ def main():
 
         with open(f'./extract_doc/{i[0].replace("/","-").replace(".","")}.txt', 'w') as n_file:
             n_file.write(data)
-
+    system('clear')
     for i in list_:
         try:
             run2(i)
         except Exception as exc: 
+            print(f'ERRO -> {exc}\n')
             pass
 
 if __name__ == "__main__":
